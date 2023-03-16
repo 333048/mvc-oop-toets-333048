7ac8ecda2223499b4911808505ced9a60321cc4e
Yulkaz <yulkazs@outlook.com>
2023-03-16 13:47:58 +0100

    php changes

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+
+class Home extends BaseController
+{
+    public function index($id = NULL, $name = NULL)
+    {
+
+        $data = [
+            'title' => 'Top 5 actiefste vulkanen ter wereld',
+            'id'    => $id,
+            'name'  => $name,
+        ];
+
+        $this->view('../index.php', $data);
+    }
+}
--- index.php
+++ index.php
@@ -1,16 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC OOP - Toets</title>
+</head>
+<body>
+    <h3><?= $data['title']; ?></h3>
 
-class Home extends BaseController
-{
-    public function index($id = NULL, $name = NULL)
-    {
+    <table border="1">
+        <thead>
+            <th>Naam Vulkaan</th>
+            <th>Hoogte (m)</th>
+            <th>Land</th>
+            <th>]Laatste Uitbarsting</th>
+            <th>Aantal Slachtoffers</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?= $data['Id']; ?></td>
+                <td><?= $data['Naam']; ?></td>
+                <td><?= $data['Hoogte']; ?></td>
+                <td><?= $data['Land']; ?></td>
+                <td><?= $data['JaarLaatsteUitbarsting']; ?></td>
+                <td><?= $data['AantalSlachtoffers']; ?></td>
+            </tr>
+        </tbody>
+    </table>
 
-        $data = [
-            'title' => 'Top 5 actiefste vulkanen ter wereld',
-            'id'    => $id,
-            'name'  => $name
-        ];
-
-        $this->view('home/index', $data);
-    }
-}
+</body>
+</html>
